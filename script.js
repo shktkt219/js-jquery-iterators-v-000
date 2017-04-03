6fe20516fcce63cd6265b1ab6e2aa363387c4eae
shoko furushita <shktkt219@gmail.com>
2017-04-03 10:31:06 -0400

    add example

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 
+  $('div').each(function(index, div){
+    $(this).append("this is div number " + (index + 1));
+  });
 
+  listIterate();
 });
+
+function listIterate(){
+return $('li').map(function(item, index){
+    return item.innerHTML;
+});
+}
